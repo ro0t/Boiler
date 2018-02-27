4d3b5eb13eb5daee908f96d5f21fcc1b6b8fcc30
Adam <adam@igital.co>
2018-02-27 11:32:12 +0100

    Sass framework, with emojis 🤪

--- config.js
+++ config.js
@@ -1,13 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const colors = require('colors/safe');
+const log = (title, data) => console.log(title, data);
+
 module.exports = env => {
 
-    console.log('Environment', env);
+    log(colors.rainbow('Environment'), colors.bold(colors.white(">> " + env)));
 
-    return {
-        entry: './app/bootstrap.js',
-        output: {
-            filename: 'boilertal-build.js',
-            path: path.resolve(__dirname, 'public')
+    let extractPlugin = new ExtractTextPlugin({
+        filename: '../css/style.css'
+    });
+
+    let cleanConfig = {
+        paths: ['cache', 'public'],
+        options: {
+            root: __dirname,
+            verbose: (env === 'dev'),
+            watch: false,
+            allowExternal: false
         }
     };
+
+    var config = {
+        mode: (env === 'dev') ? 'development' : 'production',
+        entry: ['./app/bootstrap.js', './app/style/beautiful.scss'],
+        output: {
+            filename: 'app.js',
+            path: path.resolve(__dirname, 'public/js')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: path.resolve(__dirname, './node_modules'),
+                    loader: 'babel-loader',
+                    options: { presets: ['env'] },
+                },
+                {
+                    test: /\.scss$/,
+                    use: extractPlugin.extract({
+                        use: [
+                            { loader: 'css-loader', options: { minimize: (env === 'dev') } },
+                            { loader: 'sass-loader' }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(cleanConfig.paths, cleanConfig.options),
+            extractPlugin
+        ]
+    };
+
+    return config;
 
 };
